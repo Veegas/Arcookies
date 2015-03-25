5cd4c599d113c1154838f09b7f544fe66a4d45ce
Mohamed Yasser <mohamed.yasser.abdel.razek@gmail.com>
2015-03-25 19:56:45 +0200

    Added a little bit in Page and Table

--- src/Arcookies/Page.java
+++ src/Arcookies/Page.java
@@ -1,7 +1,46 @@
 package Arcookies;
 
-public class Page  implements java.io.Serializable{
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+
+
+public class Page  implements java.io.Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//2D array of comparable
+	ArrayList<Comparable [] >tuples;
+	/*El page_id hayeb2a set men el table whenever a new page is create 
+	it's id will be "[tablename]_[page number], example student_1 page number will be incremented every time "*/	
+	String page_id;
+	
+	public void saveToDisk() throws IOException {
+		FileOutputStream fileOut =
+		         new FileOutputStream(page_id + ".class");
+		         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		         out.writeObject(this);
+		         out.close();
+		         fileOut.close();
+	}
+	
+	public Page loadFromDisk() throws ClassNotFoundException, IOException {
+		FileInputStream fileIn = new FileInputStream(page_id + ".class");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        Page page = (Page) in.readObject();
+        in.close();
+        fileIn.close();
+        return page;
+	}
+	
+	
+	
 	
 	
 
--- src/Arcookies/Table.java
+++ src/Arcookies/Table.java
@@ -5,21 +5,7 @@ import java.util.ArrayList;
 public class Table {
 
 	private String tableName;
-
-	public Table() {
-			tableName = "";
-	}
-
-	public Table(ArrayList<Column> fields) {		
-		tableName = "";
-	}
-
-	public String getTableName() {
-		return tableName;
-	}
-
-	public void setTableName(String name) {
-		tableName = name;
-	}
-
+	private ArrayList<Page> pages;
+	
+	
 }
